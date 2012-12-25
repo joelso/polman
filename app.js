8c659a1010edb49bc258e2722e51cf7a1cfdebba
Joel Soderstrom <joel.soderstrom@gmail.com>
2012-12-25 22:14:02 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    //routes = require('./routes'),
+    http = require('http'),
+    path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(require('less-middleware')({ src: __dirname + '/public' }));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+
+// Routes and handlers
+app.get('/', function(req, res){
+  res.render('index', { title: 'Express' });
+});
+
+app.post('/', function(req, res){
+  res.send('creating');
+});
+
+app.get('/w/:location', function(req, res) {
+  console.log('Will render location', req.params.location);
+});
+
+
+// Create server
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
