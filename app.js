1190b896b8dae2808d556334fcd9e2fb8141298c
Joel Soderstrom <joel.soderstrom@gmail.com>
2012-12-25 23:59:19 +0100

    Reddis

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ var express = require('express'),
     //routes = require('./routes'),
     http = require('http'),
     path = require('path'),
-    redis;
+    redis,
+    Cache;
+
+// How long we keep weather data in cache
+var WEATHER_CACHE_TTL = 60*15;
 
 var app = express();
 
@@ -38,25 +42,90 @@ app.post('/', function(req, res){
   res.send('creating');
 });
 
-app.get('/w/:location', function(req, res) {
-  console.log('Will render location', req.params.location);
+app.get('/w', function(req, res) {
+  var weatherUrl = req.query.url;
 
+  if(!weatherUrl) {
+    return res.send(400);
+  }
 
+  weatherUrl = weatherUrl.replace('http://', '');
 
+  Cache.getOrFetch(weatherUrl, function(err, xml) {
+    if(err) {
+      res.send("TODO: Proper err response");
+    }
+    res.send(xml);
+  });
 });
 
 
 // Create server
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
+  Cache.initialize();
+});
 
-  if (process.env.REDISTOGO_URL) {
-    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-    redis = require("redis").createClient(rtg.port, rtg.hostname);
 
-    redis.auth(rtg.auth.split(":")[1]);
-  } else {
-    redis = require("redis").createClient();
+// YR Client
+YR = {
+
+  fetch: function(url, cb) {
+    http.get(this.parseUrl(url), function(res) {
+      var str = '';
+      res.on('data', function (chunk) {
+        str += chunk;
+      });
+    
+      res.on('end', function () {
+        Cache.set(url, str);
+        cb.call(this, undefined, str);
+      });
+
+      res.on('error', function () {
+        cb.call(this, "Could not fetch data from yr.no");
+      });
+    
+    }).end();
+  },
+
+  parseUrl: function(url) {
+    return {
+      host: 'www.yr.no',
+      path: url.slice(url.indexOf('/'), url.length)
+    };
   }
 
-});
+};
+
+// Redis
+
+Cache = {
+  
+  initialize: function() {
+    if (process.env.REDISTOGO_URL) {
+      var rtg = require("url").parse(process.env.REDISTOGO_URL);
+      redis = require("redis").createClient(rtg.port, rtg.hostname);
+      redis.auth(rtg.auth.split(":")[1]);
+    } else {
+      redis = require("redis").createClient();
+    }
+  },
+
+  getOrFetch: function(key, cb) {
+    redis.get(key, function(err, value) {
+      if(!value) {
+        YR.fetch(key, cb);
+      } else {
+        cb.call(this, undefined, value);
+      }
+    });
+  },
+
+  set: function(key, value) {
+    redis.set(key, value, function() {
+      redis.expire(key, WEATHER_CACHE_TTL);
+    });
+  }
+
+};
